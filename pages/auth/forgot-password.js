fc0d7afd6c3adc9f42b4d589d6d58af0f08f7453
hussainalee <hussainalee@icloud.com>
2022-11-15 19:36:19 +0500

    forge-password design completed.

---
+++ pages/auth/forgot-password.js
@@ -0,0 +1,62 @@
+import Head from "next/head";
+
+export default function ForgotPassword() {
+  return (
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <Head>
+        <title>Todo - Forgot Password</title>
+        <meta type="description" content="Todo text decription" />
+      </Head>
+
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="/"
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img class="w-8 h-8 mr-2" src="/favicon.ico" alt="logo" /> todo
+        </a>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              I forgot my password
+            </h1>
+            <form class="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+
+              <button
+                type="submit"
+                class=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Send password reset send
+              </button>
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Back to,{" "}
+                <a
+                  href="/auth/sign-in"
+                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign in
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
